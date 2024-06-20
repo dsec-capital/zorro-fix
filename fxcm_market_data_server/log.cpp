f0ed30a6a14873187c98c096d4b91a89cad6db8f
Daniel Egloff <daniel@flink.ai>
2024-06-20 17:20:18 +0800

    adding path for log file

--- fxcm_market_data_server/log.cpp
+++ fxcm_market_data_server/log.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 
+#include <stdlib.h>
 #include <format>
 #include <filesystem>
 
@@ -14,11 +15,18 @@ namespace fxcm {
     std::shared_ptr<spdlog::logger> create_logger(const std::string& logger_file, spdlog::level::level_enum log_level) {
         auto cwd = std::filesystem::current_path().string();
 
+        size_t len;
+        char log_path[4096];
+        getenv_s(&len, log_path, sizeof(log_path), "FXCM_MAKRET_DATA_SERVER_LOG_PATH");
+        auto full_log_file_name = len > 0
+            ? std::format("{}\\{}", log_path, logger_file)
+            : logger_file;
+      
         auto logger_name = "default";
 
         std::vector<spdlog::sink_ptr> sinks{
             std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
-            std::make_shared<spdlog::sinks::basic_file_sink_mt>(logger_file),
+            std::make_shared<spdlog::sinks::basic_file_sink_mt>(full_log_file_name),
         };
         auto spd_logger = std::make_shared<spdlog::logger>(logger_name, begin(sinks), end(sinks));
         spdlog::register_logger(spd_logger);
@@ -26,7 +34,7 @@ namespace fxcm {
         spdlog::set_level(log_level);
         spdlog::flush_every(std::chrono::seconds(1));
 
-        spdlog::debug("Logging started, logger_name={}, logger_file={}, level={}, cwd={}", logger_name, logger_file, (int)spd_logger->level(), cwd);
+        spdlog::debug("Logging started, logger_name={}, logger_file={}, level={}, cwd={}", logger_name, full_log_file_name, (int)spd_logger->level(), cwd);
 
         return spd_logger;
     }
@@ -43,18 +51,26 @@ namespace fxcm {
         auto cwd = std::filesystem::current_path().string();
 
         auto sym = normalize_symbol(symbol);
-        auto logger_name = std::format("{}_{}", sym, tag);
         auto logger_file = std::format("{}_{}_{}.log", sym, tag, common::timestamp_postfix());
 
+        size_t len;
+        char log_path[4096];
+        getenv_s(&len, log_path, sizeof(log_path), "FXCM_MAKRET_DATA_SERVER_LOG_PATH");
+        auto full_log_file_name = len > 0
+            ? std::format("{}\\{}", log_path, logger_file)
+            : logger_file;
+
+        auto logger_name = std::format("{}_{}", sym, tag);
+
         std::vector<spdlog::sink_ptr> sinks{
-            std::make_shared<spdlog::sinks::basic_file_sink_mt>(logger_file),
+            std::make_shared<spdlog::sinks::basic_file_sink_mt>(full_log_file_name),
         };
         auto data_logger = std::make_shared<spdlog::logger>(logger_name, begin(sinks), end(sinks));
         spdlog::register_logger(data_logger);
         spdlog::set_level(log_level);
         spdlog::flush_every(std::chrono::seconds(1));
 
-        spdlog::debug("Data logger created, logger_file={}, level={}, cwd={}", logger_file, (int)data_logger->level(), cwd);
+        spdlog::debug("Data logger created, logger_file={}, level={}, cwd={}", full_log_file_name, (int)data_logger->level(), cwd);
 
         return data_logger;
     }
